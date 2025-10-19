ff6b2164d440587934a896f1cbd169befeb2d2e5
Felicite37 <feliciteniyo24@gmail.com>
2025-10-19 14:51:18 +0200

    I added Footer.jsx in components

---
+++ Footer.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Footer() {
+  return (
+    <footer className="bg-green-600 text-white text-center py-4 mt-10">
+      &copy; {new Date().getFullYear()} Joyful Bites Store. All rights reserved.
+    </footer>
+  );
+}
